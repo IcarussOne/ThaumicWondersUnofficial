3b1817a147ef7a66d576e948c36bb03d3cfc6b3e
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-05-22 14:37:20 -0500

    Rework enchant zap sounds

--- src/main/java/com/verdantartifice/thaumicwonders/client/renderers/tile/TesrEssentiaEnchanter.java
+++ src/main/java/com/verdantartifice/thaumicwonders/client/renderers/tile/TesrEssentiaEnchanter.java
@@ -2,6 +2,7 @@ package com.verdantartifice.thaumicwonders.client.renderers.tile;
 
 import com.verdantartifice.thaumicwonders.common.blocks.devices.BlockEssentiaEnchanter;
 import com.verdantartifice.thaumicwonders.common.blocks.misc.BlockArcanePillar.EnumDirection;
+import com.verdantartifice.thaumicwonders.common.registry.SoundsTW;
 import com.verdantartifice.thaumicwonders.common.tiles.devices.TileEssentiaEnchanter;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.client.Minecraft;
@@ -17,7 +18,6 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import thaumcraft.client.fx.FXDispatcher;
-import thaumcraft.common.lib.SoundsTC;
 
 import java.awt.*;
 
@@ -81,9 +81,13 @@ public class TesrEssentiaEnchanter extends TileEntitySpecialRenderer<TileEssenti
 
     private void renderLightning(TileEssentiaEnchanter tile, float partialTicks) {
         if(tile.getProgress() > 0) {
-            if(tile.getProgress() % (tile.getMaxProgress() / 9) == 0) {
+            if(tile.getProgress() % (tile.getMaxProgress() / 9) == 0 && !(tile.getProgress() <= 30)) {
                 tile.getWorld().playSound(tile.getPos().getX() + 0.5, tile.getPos().getY() + 0.5, tile.getPos().getZ() + 0.5,
-                        SoundsTC.jacobs, SoundCategory.BLOCKS, 0.5f, 1.0f, false);
+                        SoundsTW.ENCHANT_ZAP, SoundCategory.BLOCKS, 0.5f, 1.0f, false);
+            }
+            if(tile.getProgress() == 30) {
+                tile.getWorld().playSound(tile.getPos().getX() + 0.5, tile.getPos().getY() + 0.5, tile.getPos().getZ() + 0.5,
+                        SoundsTW.ENCHANT_END, SoundCategory.BLOCKS, 0.5f, 1.0f, false);
             }
             int pillars = (int) ((1.0 - (double) tile.getProgress() / tile.getMaxProgress()) * 9.0);
             Vec3d center = new Vec3d(tile.getPos().getX() + 0.5, tile.getPos().getY() + 1.15, tile.getPos().getZ() + 0.5);
